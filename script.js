0a7ec13e2716ef8526f05c32146d52f34ac7b2f8
RSE77 <rysboards@yahoo.com>
2021-03-17 12:56:09 -0400

    added functionality to Tweet button. Added loading page icon for waiting on fetch request

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
+const myButton = document.getElementById("new-quote");
+const twitterButton = document.getElementById("twitter");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const loader = document.getElementById("loader");
+const quoteContainer = document.getElementById("quote-container");
+
 // Get Quotes From API
 let apiQuotes = [];
 
-function getNewQuote() {
-    const quote = apiQuotes[Math.random(Math.floor() * apiQuotes.length)];
-    console.log(quote);
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
 }
 
 async function getQuotes() {
-    try {
-    
+    loading();
     const apiUrl = 'https://api.quotable.io/random';
+    try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    getNewQuote();
-    console.log(apiQuotes)
+    complete();
 //Catch Error Here 
      } catch(error) {
     alert('error occured');
 }}
+
+function newQuote() {
+    loading();
+    myButton.onclick = getQuotes;
+    
+    quoteText.textContent = `${apiQuotes.content}`;
+    authorText.textContent = `${apiQuotes.author}`;
+    complete();
+}
+
+//Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+
+}
+
+myButton.addEventListener("click", newQuote);
+twitterButton.addEventListener("click", tweetQuote);
 
 //On Load
 getQuotes();
